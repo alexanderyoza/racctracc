4d76dbe38fbe718b17d755bd920224ff7a311eab
catliba <1caleblili@gmail.com>
2023-05-31 02:58:40 -0700

    heroku

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -30,11 +31,11 @@ const uploadRouter = require('./backend/routes/uploadRoutes');
 app.use('/api/users', userRouter);
 app.use('/api/uploads', uploadRouter);
 
-// deployment
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
-// }
+//  deployment
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('client/build'));
+        app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+    }
 
 // port
 const port = process.env.PORT || 9000;
